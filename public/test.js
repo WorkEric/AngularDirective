f523460da17bd2269db36c63056d9adabdb20e59
WorkEric <ruiwmichael@gmail.com>
2016-03-23 17:18:25 -0400

    new directive

---
+++ public/test.js
@@ -0,0 +1,19 @@
+(function(){
+    angular.module('test', [])
+        .controller('ctrl', function($scope){
+        
+        })
+        .directive('myDir', function(){
+            return {
+                template: '<input ng-model="info">',
+                scope: {
+                    info: '<'
+                },
+                controller: function() {
+                    console.log('my ctrl controller', this);
+                }
+            }
+        });
+
+
+})();
